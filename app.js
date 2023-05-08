c409beae2de03e8e322e50744bd9cc7bfa2adf46
gwendyl <gwendyl@gmail.com>
2023-05-08 18:00:14 -0400

    login process reasonably done

--- app.js
+++ app.js
@@ -1,119 +1,39 @@
-//NOTES TO SELF.  Consider refactor to move authentication logic to a middle layer.  See https://www.loginradius.com/blog/engineering/guest-post/nodejs-authentication-guide/
-// think about where the cookie logic needs to be for the password change functionality
-
 require('dotenv').config()
-
-const express = require('express');
-const ejs = require('ejs');
-const {MongoClient} = require('mongodb');
-const mongoose = require('mongoose');
+const express = require('express')
+    , session = require('express-session')
+    , ejs = require('ejs')
 //const session = require('express-session');
-const crypto = require("crypto");
-const sendEmail = require("./utils/emails/sendEmail");
 
-// JSON Web Token helps shield a route from an unauthenticated user. Using JWT in your application will prevent unauthenticated users from accessing your users' home page and prevent unauthorized users from accessing your admin page.
-// JWT creates a token, sends it to the client, and then the client uses the token for making requests. It also helps verify that you're a valid user making those requests.
-const jwt = require('jsonwebtoken')
+const users = require('./controllers/users');
+
 // To prevent unauthenticated users from accessing the private route, take the token from the cookie, verify the token, and redirect users based on role.
 // You'll get the token from the client using a node package called cookie-parser. 
-const cookieParser = require("cookie-parser");
+//const cookieParser = require("cookie-parser");
 
 //const passport = require("passport"); 
 //const passportLocalMongoose = require("passport-local-mongoose");
 
-const md5 = require("md5");
-
 const port = process.env.PORT;
- 
-const app = express();
- 
+const app = express(); 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-
-// app.use(session({
-//     secret: process.env.PWDSECRET,
-//     resave: false,
-//     saveUninitialized: false
-//   }));
-//   app.use(passport.initialize());
-//   app.use(passport.session());
-
-
-
-const uri = "mongodb+srv://"+process.env.MONGODBCRED+"@mosaic1.ul7isoy.mongodb.net/?retryWrites=true&w=majority"; 
-
-const userSchema = new mongoose.Schema(
-{
-    email: {type: String, trim: true, required: true, unique: true,},
-    password: String
-},
-{
-  timestamps: true,
-},
-{
-    collection: 'users'
-});
-
-//userSchema.plugin(passportLocalMongoose);
-//userSchema.plugin(findOrCreate);
-
-const User = mongoose.model('User', userSchema);
-//passport.use(User.createStrategy());
-//passport.serializeUser(function(user, done) {
-//    done(null, user.id);
-//  });
-  
-const tokenSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "user",
-  },
-  token: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: 3600,// this is the expiry time in seconds
-  },
-});
-const Token = mongoose.model('Token', tokenSchema);
-
-
-
-
-
-// passport.deserializeUser(function(id, done) {
-//     User.findById(id, function(err, user) {
-//       done(err, user);
-//     });
-//   });
-
-
-main().catch(err => console.log(err));
-async function main() {
-    const options = {
-        dbName: 'mosaiccrochet'
-    }
-    await mongoose.connect(uri, options);
-}
-
-
+//app.use(cookieParser());
+app.use(
+    session({
+        secret: process.env.SESSIONSECRET,
+        cookie: { maxAge: 30000 },
+        resave: true,
+        saveUninitialized: false,  // critical
+    })
+);
 
 /////////////////////////////////
-//  main routes
+//  start server
 /////////////////////////////////
-app.get("/", function (req, res) {
-    res.render("home");
-} );
-
 const server = app.listen(port, () => {
     console.log(`Server started today on port ${port}`);
-  });
+});
 // Handling Error
 process.on("unhandledRejection", err => {
     console.log(`An error occurred: ${err.message}`)
@@ -122,270 +42,36 @@ process.on("unhandledRejection", err => {
 
 
 /////////////////////////////////
-// Login logic
+// routes
 /////////////////////////////////
+//app.get("/", function (req, res) {users.renderContent(req, res)});
+app.get("/", function (req, res) { users.renderHome(res,"")});
 
-app.get("/login", function (req, res) {
-    res.render("login", {userMsg: "no message"});
-}  );
+app.get("/login", function (req, res) {users.renderLogin(res,"")});
+app.post("/login", async function (req, res) {users.login(req,res);});
 
-app.post("/login", async function (req, res) {
-    
-    const email = req.body.username;
-    const password = md5(req.body.password);
-    if (!email || !password) {
-        return res.status(400).json({
-          message: "Username or Password not present",
-        })
-      }
+app.get("/register", function (req, res) {users.renderRegister(res,"")} );
+app.post("/register", async function(req, res) {users.register(req, res);});
 
+app.get("/registerpassword", function (req, res) {users.renderRegisterPassword(req, res,"")} );
+app.post("/registerpassword", function (req, res) {
+    users.registerPassword(req, res,"")
+} );
 
-
-    try {
-        const foundUser = await User.findOne({email, password});
-    if (!foundUser) {
-          res.status(401).json({
-            message: "Login not successful",
-            error: "User not found",
-          })
-        } else {
-            const maxAge = 3*60*60; // 3 hours in seconds
-            const token = jwt.sign(
-                {id:foundUser._id, email},
-                process.env.JWTSECRET,
-                { expiresIn: maxAge }
-            );
-            res.cookie("jwt", token, {
-                httpOnly: true,
-                maxAge: maxAge * 1000 // ms
-            });
-          res.status(201).json({
-            message: "Login successful",
-            user: foundUser._id,
-          });
-        }
-      } catch (error) {
-        res.status(400).json({
-          message: "An error occurred",
-          error: error.message,
-        })
-      }
-
-
-
-//     if(!foundUser) {
-//         // no such login id
-//         console.log('no such username');
-//         res.render("login", {userMsg: "Incorrect Email or Password"});
-        
-//     } else {
-//         console.log('check pwd agaisnt ' + foundUser.password);
-//         if(foundUser.password!=md5(req.body.password)) {
-//             console.log('incoorect passwrd: '+ foundUser.password + ' vs ' + md5(req.body.password));
-//             res.render("login", {userMsg: "Incorrect Email or Password"});
-//         } else {
-//             console.log('successful login');
-//  //           passport.authenticate("local")(req, res, function(){
-//                 res.redirect("/content");
-//  //             });
-            
-//         }
-//     } 
+app.get("/resetpassword", function (req, res) {users.renderResetPassword(req, res,"")});
+app.post("/resetpassword", async function(req, res) {
+    users.resetPassword(req,res);
 });
 
-/////////////////////////////////
-// Register logic
-/////////////////////////////////
-
-app.get("/register", function (req, res) {
-    res.render("register");
-}  );
-
-app.post("/register", async function(req, res, next) {
-
-    const email = req.body.username;
-    const password = md5(req.body.password);
-
-    if (password.length < 6) {
-        return res.status(400).json({ message: "Password less than 6 characters" })
-      }
-      try {
-        await User.create({
-            email,
-          password,
-        }).then(user => {
-          const maxAge = 3 * 60 * 60; // 3 hours in seconds
-          const token = jwt.sign(
-            { id: user._id, email },
-            process.env.JWTSECRET,
-            {
-                expiresIn: maxAge
-            }
-          );
-          res.cookie("jwt", token, {
-            httpOnly: true,
-            maxAge: maxAge * 1000 // ms
-          });
-          res.status(200).json({
-            message: "User successfully created",
-            user: user._id,
-          });
-      })
-      } catch (error) {
-        res.status(400).json({
-          message: "User not successful created",
-          error: error.message,
-        })
-      }
-
-
-
-
-
-//     const foundUser = await findUser(req.body.username);
-//     if(foundUser) {
-//         res.render("login", {userMsg: "Email already exists"});
-//         console.log('username already exists');
-//     } else {
-//     if(req.body.password.length < 6) {
-//         return res.status(400).json({ message: "Password less than 6 characters" })
-//     } else {
-//     if(!foundUser) {
-//         console.log('converting ' + req.body.password);
-//         if(saveUser(req.body.username, md5(req.body.password))) {
-//  //           passport.authenticate("local")(req, res, function(){
-//                 res.render("secrets")
-//  //           })
-//         }
-//     } }}
+app.get("/changepassword", function (req, res) {users.renderChangePassword(req, res,"")});
+app.post("/changepassword", async function(req, res) {
+    users.changePassword(req,res);
 });
 
-/////////////////////////////////
-// Change Password Logic
-/////////////////////////////////
+app.get("/resetlink", async function(req, res) {users.renderSendLinkForm(res,"")});
+app.post("/resetlink", async function(req, res) {users.sendResetLink(req, res);});
 
-app.get("/passwordchange", function (req, res) {
-    res.render("passwordchange", {userMsg: "No Message Yet"});
-}  );
+app.get("/logout", function (req, res) {users.logout(req, res);});
 
-app.post("/passwordchange", async function(req, res) {
-    const foundUser = await findUser(req.body.username);
-    if(!foundUser) {
-        // no such login id
-        console.log('no such username');
-        res.render("passwordchange", {userMsg: "Incorrect Email or Password"});
-        
-    } else {
-        console.log('check pwd agaisnt ' + foundUser.password);
-        if(foundUser.password!=md5(req.body.oldpassword)) {
-            console.log('incoorect passwrd');
-            res.render("passwordchange", {userMsg: "Incorrect Email or Password"});
-        } else {
-            if(updatePwd(req.body.username, md5(req.body.newpassword))) {
-                res.render("secrets")
-            }
-        }
-    } 
-});
+app.get("/content", function (req, res) {users.renderContent(req,res)});
 
-/////////////////////////////////
-// Request Password reset Link logic
-/////////////////////////////////
-app.get("/resetlink", async function(req, res) {
-  res.render("resetlink", {userMsg: "No Message Yet"});
-});
-
-app.post("/resetlink", async function(req, res) {
-  console.log("post resetlink route for "+req.body.username);
-  const user = await User.findOne({ email: req.body.username });
-  if (!user) {
-    console.log('no such username');
-    res.render("resetlink", {userMsg: "Email does not exist."});
-  }
-  
-  let token = await Token.findOne({ userId: user._id });
-  if (token) {
-    console.log('deleting found prior token ' + token)
-    await token.deleteOne();
-  }
-  let resetToken = crypto.randomBytes(32).toString("hex");
-  console.log('created token ' + resetToken);
-  const hash =md5(resetToken);
-  console.log('hashed token ' + hash);
-  
-  await new Token({
-    userId: user._id,
-    token: hash,
-    createdAt: Date.now(),
-  }).save();
-
-  const clientURL = 'localhost://'+process.env.PORT;
-  const link = `${clientURL}/passwordReset?token=${resetToken}&id=${user._id}`;
-  console.log('created link ' + link);
-  sendEmail(user.email,"Password Reset Request",{name: user.email,link: link,},"./template/requestResetPassword.handlebars");
-  return link;
-});
-/////////////////////////////////
-// Logout logic
-/////////////////////////////////
-
-app.get("/logout", function (req, res) {
-    res.cookie("jwt", "", { maxAge: "1" })
-    res.redirect("/");
-}  );
-
-/////////////////////////////////
-// Content logic
-/////////////////////////////////
-
-app.get("/content", function (req, res, next) {
-    userAuth(req, res, next);
-    res.render("secrets");
-
-//   User.find({"secret": {$ne: null}}, function(err, foundUsers){
-//     if (err){
-//       console.log(err);
-//     } else {
-//       if (foundUsers) {
-//         res.render("secrets", {usersWithSecrets: foundUsers});
-//       }
-//     }
-//   });
-});
-
-/////////////////////////////////
-// Functions
-/////////////////////////////////
-
-async function saveUser(userName, password) {
-    const newUser = new User({ email: userName, password:password });
-    await newUser.save();
-}
-
-async function updatePwd(userName, newPassword) {
-    console.log('creating new record for ' + userName + ' ' + newPassword);
-    return await User.updateOne({email:userName}, {password: newPassword});
-}
-
-
-async function isLoggedIn(){
-    return false;
-}
-
-
-function userAuth(req, res, next) {
-    const token = req.cookies.jwt
-    if (token) {
-      jwt.verify(token, process.env.JWTSECRET, (err, decodedToken) => {
-        if (err) {
-          return res.status(401).json({ message: "Not authorized 1" })
-        } else {
-          next();
-        }
-      })
-    } else {
-      return res
-        .status(401)
-        .json({ message: "Not authorized, token not available" })
-    }
-  }
---
+++ controllers/users.js
@@ -0,0 +1,361 @@
+const {MongoClient} = require('mongodb')
+    , mongoose = require('mongoose')
+    , md5 = require("md5")
+    , crypto = require("crypto")
+    , sendTemplateEmail = require('../utils/emails/sendEmail')
+    ;
+
+
+// JSON Web Token helps shield a route from an unauthenticated user. Using JWT in your application will prevent unauthenticated users from accessing your users' home page and prevent unauthorized users from accessing your admin page.
+// JWT creates a token, sends it to the client, and then the client uses the token for making requests. It also helps verify that you're a valid user making those requests.
+const jwt = require('jsonwebtoken')
+
+/////////////////////////////////////
+// DB Models
+////////////////////////////////////
+const userSchema = new mongoose.Schema(
+{
+    email: {type: String, trim: true, required: true, unique: true,},
+    password: String
+},
+{
+    timestamps: true,
+},
+{
+    collection: 'users'
+});
+const User = mongoose.model('User', userSchema);
+    
+
+const tokenSchema = new mongoose.Schema({
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    regEmail: {
+        type: String
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+    tokenType:
+    { 
+        type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      expires: 3600,// this is the expiry time in seconds
+    },
+  });
+  const Token = mongoose.model('Token', tokenSchema);
+  
+  
+  
+
+/////////////////////////////////////
+// export functions
+////////////////////////////////////
+  
+exports.login = async (req, res) => {
+
+    const email = req.body.username;
+    const password = md5(req.body.password);
+    
+    if (!email || !password) {
+        this.renderLogin(res, "Username or Password not present")
+        return
+    }
+
+    try {
+        const foundUser = await User.findOne({email, password});
+    if (!foundUser) {
+        // no such login id
+        this.renderLogin(res, "Incorrect Email or Password");
+        } else {
+
+            // create session
+            req.session.authenticated = true;
+            req.session.user = email;
+            //res.json(req.session);
+
+            this.renderContent(req, res, "");
+
+        }
+    } catch (error) {
+        this.renderLogin(res,error.message);
+    }
+};
+
+
+exports.register = async (req, res) => {
+    const email = req.body.username;
+    try {
+        const foundUser = await User.findOne({email});
+        if (foundUser) {
+            this.renderLogin(res, "Email already registered.  Please log in or reset password.");
+            return
+        }
+
+        // clean up old token ??? should it be cleaning up all tokens?
+        let token = await Token.findOne({ regEmail: email });
+        if (token) await token.deleteOne();
+
+        // create new registration token
+        let regToken = crypto.randomBytes(32).toString("hex");
+        await new Token({
+        regEmail: email,
+        token: regToken,
+        tokenType: "userRegister",
+        createdAt: Date.now(),
+        }).save();
+    
+        const link = buildSetPasswordURL(regToken, email,"");
+        sendTemplateEmail(email, "d-6f4569924db3494d910b2fde1bbf4a55", link);
+        this.renderHome(res, "Please check your email for the link to complete account setup")
+        return;
+    
+    } catch (error) {
+        this.renderRegister(res, error.message);
+        return
+    }
+}
+
+
+exports.changePassword = async (req, res) => {
+    const email = req.body.username;
+    const password = md5(req.body.oldpassword);
+    const newpassword = req.body.newpassword;
+    const newpassword2 = req.body.newpassword2;
+
+    if (req.body.newpassword.length < 6) {
+        this.renderChangePassword(req, res, "Password less than 6 characters.  Password not changed.");
+        return
+    } 
+
+    if (newpassword != newpassword2) {
+        this.renderChangePassword(req, res, "New passwords do not match. Password not changed.");
+        return;
+    }
+
+    const foundUser = await User.findOne({email, password});
+    if(!foundUser) {
+        // no such login id
+        this.renderChangePassword(req, res,"Incorrect Email or Password.  Password not changed.");
+        return
+    } 
+
+    if(foundUser.password!=password) {
+        this.renderChangePassword(req, res,"Incorrect Email or Password.  Password not changed.");
+        return
+    } 
+
+    await User.updateOne({email:email}, {password: md5(newpassword)});
+
+    // reset session - I don't know if this is working yet
+    req.session.authenticated = true;
+    req.session.user = email;
+
+    this.renderContent(req, res)
+} 
+
+
+exports.resetPassword = async (req, res) => {
+    const email = req.body.username;
+    const newpassword = md5(req.body.newpassword);
+    const newpassword2 = md5(req.body.newpassword2);
+    const tokenString = req.body.token;
+
+
+    // check for valid token
+    let regToken = await Token.findOne({token: tokenString });
+    if (!regToken) {
+        this.renderRegister(res, "Expired or invalid link.  Please enter email address for new registration email.");
+        return;
+    }
+
+    if (regToken.regEmail != email) {
+        this.renderRegister(res, "Token does not match username.  Please enter email address for new registration email.");
+        return;
+    }
+
+    if (req.body.newpassword.length < 6) {
+        res.render("resetpassword", {userMsg: "Password less than 6 characters", email: email, is_auth: false, token:tokenString});
+        return
+    } 
+
+    const foundUser = await User.findOne({email});
+    if(!foundUser) {
+        // no such login id
+        this.renderRegister(req, res,"No such email found.  Please enter email address for new registration email.");
+        return
+    } 
+
+    if(newpassword!=newpassword2) {
+        res.render("resetpassword", {userMsg: "New passwords do not match.", email: email, is_auth: false, token:tokenString});
+        return
+    } 
+
+    await User.updateOne({email:email}, {password: newpassword});
+
+    // reset session - I don't know if this is working yet
+    req.session.authenticated = true;
+    req.session.user = email;
+
+    // delete the token that was just used
+    regToken.deleteOne();
+
+    this.renderContent(req, res)
+} 
+
+exports.registerPassword = async (req, res, usermsg) => {
+    const email = req.body.username;
+    const unhashedpassword = req.body.newpassword;
+    const password = md5(req.body.newpassword);
+    const password2 = md5(req.body.newpassword2);
+    const tokenString = req.body.token;
+
+    if (unhashedpassword.length < 6) {
+        res.render("registerpassword", {userMsg: "Password less than 6 characters", is_auth: false, email, token: tokenString});
+        return;
+    } 
+
+    if (password!=password2) {
+        res.render("registerpassword", {userMsg: "Passwords do not match", is_auth: false, email, token: tokenString});
+        return;
+    } 
+
+    const foundUser = await User.findOne({email});
+    if(foundUser) {
+        // no such login id
+        res.render("registerpassword", {userMsg: "Apologies - email already set up", is_auth: false, email, token: tokenString});
+        return;
+    } 
+
+    let regToken = await Token.findOne({ regEmail: email, token: tokenString });
+    if (!regToken) {
+        this.renderRegister(res, "Expired or invalid link.  Please enter email address for new registration email.");
+        return;
+    }
+
+    // establish session - I don't know if this is working yet
+    req.session.authenticated = true;
+    req.session.user = email;
+
+    try {
+        // delete registration token
+        await regToken.deleteOne();
+        this.renderContent(req, res, "");
+    } catch (error) {
+        this.renderRegister(res, error.message);
+    }
+
+}
+
+exports.sendResetLink = async (req, res) => {
+    const user = await User.findOne({ email: req.body.username });
+    if (!user) {
+       this.renderSendLinkForm(res, "Email is not registered. Please register!");
+        return
+    }
+
+    let priorResetToken = await Token.findOne({ userId: user._id });
+    if (priorResetToken) await priorResetToken.deleteOne();
+
+    let resetToken = crypto.randomBytes(32).toString("hex");
+
+    await new Token({
+    userId: user._id,
+    regEmail: req.body.username,
+    token: resetToken,
+    tokenType: 'resetPassword',
+    createdAt: Date.now(),
+    }).save();
+
+    const clientURL = 'http://localhost:'+process.env.PORT;
+    const link = `${clientURL}/resetpassword?token=${resetToken}&id=${user._id}`;
+    sendTemplateEmail(user.email, "d-a31f1268bfcc40cf856ef2f107339e49", link);
+    this.renderLogin(res, "Please check your email for the link to reset your password")
+    return;
+}
+
+exports.authorized = (req, res) => {
+    if(req.session.authenticated) {
+        res.render("secrets", { is_auth: true });
+    } else {
+        this.renderLogin(res, "");
+    }
+        
+  }
+
+
+exports.logout = (req, res) => {
+    // reset session - I don't know if this is working yet
+    req.session.authenticated = false;
+    res.redirect("/");
+}
+
+//////////////////////////////
+// render functions
+//////////////////////////////
+exports.renderHome = (res, usermsg) => {
+    res.render("home", {userMsg: usermsg, is_auth: false});
+}
+exports.renderContent = (req, res) => {
+    this.authorized(req, res);
+}
+exports.renderLogin = (res, usermsg) => {
+    res.render("login", {userMsg: usermsg, is_auth: false});
+}
+
+exports.renderRegister = (res, usermsg) => {
+    res.render("register", {userMsg: usermsg, is_auth: false});
+}
+exports.renderRegisterPassword = (req, res, usermsg) => {
+    res.render("registerpassword", {userMsg: usermsg, is_auth: false, email:req.query.email, token:req.query.token});
+}
+
+exports.renderChangePassword = async (req, res, usermsg) => {
+    //obtain token details
+        // reset session - I don't know if this is working yet
+    if(!req.session.user) {
+        this.renderLogin(res, "Session expired.  Please log in");
+        return
+    }
+    res.render("changepassword", {userMsg: usermsg, email: req.session.user, is_auth: false});
+}
+exports.renderResetPassword = async (req, res, usermsg) => {
+    //obtain token details
+    let regToken = await Token.findOne({token: req.query.token });
+    if (!regToken) {
+        this.renderSendLinkForm(res,"Expired or invalid link. Enter your email to receive a new link for password reset.") 
+        return;
+    }
+    res.render("resetpassword", {userMsg: usermsg, email: regToken.regEmail, is_auth: false, token:req.query.token});
+}
+exports.renderSendLinkForm = (res,usermsg) => {
+    res.render("resetLink", {userMsg: usermsg, is_auth: false});
+}   
+
+//////////////////////////////
+// internal utility functions
+//////////////////////////////
+function buildSetPasswordURL(regToken, email, usrmsg) {
+    const clientURL = 'http://localhost:'+process.env.PORT;
+    return `${clientURL}/registerPassword?token=${regToken}&email=${email}&userMsg="${usrmsg}"`;
+}
+
+//////////////////////////////
+// database connecion
+//////////////////////////////
+const uri = "mongodb+srv://"+process.env.MONGODBCRED+"@mosaic1.ul7isoy.mongodb.net/?retryWrites=true&w=majority"; 
+
+main().catch(err => console.log(err));
+async function main() {
+    const options = {
+        dbName: 'mosaiccrochet'
+    }
+    await mongoose.connect(uri, options);
+}
+
